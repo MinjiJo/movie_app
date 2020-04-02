e46a03d5ee07331ac47792d6e64d18abb498ad6f
minjijo <story3697@gmail.com>
2020-04-03 07:28:30 +0900

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+function Potato(){
+  return <h1>I like Potato</h1>;
+}
+
 function App() {
   return (
-    <div className="App" />
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
   );
 }
 
